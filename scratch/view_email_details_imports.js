4a0d819b2727c60b51a3c0b77982d6fb6d4d5e12
btc002b.btcglobal@gmail.com <btc002b.btcglobal@gmail.com>
2026-09-02 12:45:49 +0530

    chore: remove deprecated constants from configuration module

--- scratch/view_email_details_imports.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const path = 'C:/Users/ASHWIN/Downloads/BNX-MAIL-FE/src/components/EmailDetails.jsx';
-const content = fs.readFileSync(path, 'utf8');
-const lines = content.split('\n');
-
-for (let i = 0; i < 25; i++) {
-    console.log(`L${i+1}: ${lines[i]}`);
-}
